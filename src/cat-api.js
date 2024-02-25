ee76e30db3be4bf3ebe0ffcfbea1edc8bf62652a
Homrts620 <hom620@gmail.com>
2024-02-25 12:29:06 +0100

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchBreeds = () => {
 axios.defaults.headers.common['x-api-key'] =
     'live_vwW8oNfBaqOPjIqE0ap1tBOa1IU3JSSGfWnv7rTeDZu8WNvmsMl7ukGjRazeKIWZ';
-    
+
 return axios.get(`https://api.thecatapi.com/v1/breeds`).then(res => res.data);
 };
 
