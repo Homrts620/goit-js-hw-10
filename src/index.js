877cc6f0773abe07463e0af424e74869b87ec1ac
Homrts620 <hom620@gmail.com>
2024-02-25 12:12:08 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {fetchBreeds, fetchCatByBreed} from './cat-api';
 
-const breedSelect = document.querySelector('.breed-select');
+const breedSelect = document.querySelector('.breed-select')
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 
