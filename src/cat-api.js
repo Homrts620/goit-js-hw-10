e29bbc2b64560626548cfc0e3f2df4c879d5dab2
Homrts620 <hom620@gmail.com>
2024-03-01 19:39:16 +0100

    hw

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
-export const fetchBreeds = () => {
+export const fetchBreeds = async () => {
 axios.defaults.headers.common['x-api-key'] =
     'live_vwW8oNfBaqOPjIqE0ap1tBOa1IU3JSSGfWnv7rTeDZu8WNvmsMl7ukGjRazeKIWZ';
 
-return axios.get(`https://api.thecatapi.com/v1/breeds`).then(res => res.data);
+    const response = await axios.get(`https://api.thecatapi.com/v1/breeds`);
+    return response.data;
 };
 
-export const fetchCatByBreed = breedId => {
-return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(res => res.data);
+export const fetchCatBreeds = async breedId => {
+    const response = await axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`);
+    return response.data;
 };
--- src/index.js
+++ src/index.js
@@ -1,42 +1,75 @@
-import {fetchBreeds, fetchCatByBreed} from './cat-api';
+import { fetchBreeds, fetchCatBreeds} from './cat-api';
+import './styles.css';
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
 
 const breedSelect = document.querySelector('.breed-select');
-const catInfo = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
+const infoCat = document.querySelector('.cat-info');
+const loaderPage = document.querySelector('.loader');
 
+
+document.querySelector('.loader').classList.remove('loading');
+
+async function fetchData() {
 try {
-loader.classList.remove('hidden');
-fetchBreeds().then(data => renderSelect(data));
+    loaderPage.classList.remove('hidden');
+    const breedsData = await fetchBreeds();
+    renderSelect(breedsData);
+    new SlimSelect({
+    select: '#single',
+    settings: {
+        closeOnSelect: true,
+        hideSelected: true,
+        openPosition: 'auto',
+        contentPosition: 'absolute',
+    }
+    });
 } catch (error) {
-console.log(error);
+    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
 }
+}
+fetchData();
+
+document.querySelector('.error').style.display = 'none';
 
 function renderSelect(breeds) {
-const markup = breeds
+const option = breeds
     .map(({ id, name }) => {
     return `<option value="${id}">${name}</option>`;
     })
     .join('');
-breedSelect.insertAdjacentHTML('beforeend', markup);
-loader.classList.add('hidden');
+
+breedSelect.insertAdjacentHTML('beforeend', option);
+loaderPage.classList.add('hidden');
 }
 
-breedSelect.addEventListener('change', e => {
-loader.classList.remove('hidden');
-fetchCatByBreed(e.target.value).then(data => renderCat(data[0]));
-});
+breedSelect.addEventListener('change', async event => {
+document.querySelector('.loader').classList.add('loading');
+try {
+    const catData = await fetchCatBreeds(event.target.value);
+    renderCat(catData[0]);
+} catch (errorCat) {
+    document.querySelector('.loader').classList.remove('loading');
+    Notiflix.Notify.failure('Sorry, something went wrong. Please choose another cat breed.');
+    infoCat.innerHTML = '';
+}
+})
+
+document.querySelector('.errorCat').style.display = 'none';
+
 
 function renderCat(catData) {
 const { url } = catData;
-const { description, name, temperament } = catData.breeds[0];
-catInfo.insertAdjacentHTML(
-    'beforeend',
-    `<div>
-        <h2>${name}</h2>
-        <img src="${url}" alt="${name}" />
-        <p>${description}</p>
-        <p><strong>Temperament:</strong> ${temperament}</p>
-    </div>`
-);
-loader.classList.add('hidden');
+const { description, name, temperament, origin } = catData.breeds[0];
+infoCat.innerHTML = '';
+infoCat.innerHTML += `
+    <img src="${url}" alt="${name}" height="960" weight="480"/>
+    <h2>${name}</h2>
+    <p>${description}</p>
+    <p><strong>Temperament:</strong> ${temperament}</p>
+    <p>${origin}</p>`;
+
+document.querySelector('.loader').classList.remove('loading');
 }
